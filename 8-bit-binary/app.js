cb7ba897a84e1ae0522b1a4f6204b7298a857e9f
chiragdoctor <chirag.doctor@gmail.com>
2020-04-14 16:11:16 +0100

    8-bit conversion logic

--- 8-bit-binary/app.js
+++ 8-bit-binary/app.js
@@ -6,3 +6,24 @@ const sentence = readlineSync.question('Enter the sentence : ');
 
 const letters = sentence.split('');
 
+const result = letters.map(letter => {
+    const asciiValue = ascii[letter];
+    return binary(asciiValue);
+});
+
+
+function binary(asciiValue, result = '') {
+    if (asciiValue <= 1) {
+        result = asciiValue + result;
+        const noOfZeros = 8 - result.length;
+        for(let i = 0; i < noOfZeros; i++) {
+            result = '0' + result; 
+        }    
+        return result;
+    } else {
+        result = Math.floor(asciiValue % 2) + result;
+        return binary(Math.floor(asciiValue / 2), result);
+    }
+}
+
+console.log(result);
